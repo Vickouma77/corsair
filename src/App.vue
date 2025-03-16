90206b59927e90e730f1aaec30fd46721d36b8df
Vickouma77 <Vickprestone20@gmail.com>
2025-03-16 22:05:11 +0300

    chore: integrate Tailwind CSS, add Navbar component, and update TypeScript configuration

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-
+import NavbarComponent from './components/NavbarComponent.vue';
 </script>
 
 <template>
   <header>
-    
+    <NavbarComponent />
   </header>
 
   <main>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <nav class="bg-grey-800">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="absolute insert-y-0 left-0 flex items-center sm:hidden">
+                <!-- Mobile menu button -->
+                 <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
+                    <span class="absolute -inset-0.5"></span>
+                    <span class="sr-only">Open main menu</span>
+                    <!-- 
+                        Icon when menu is closed. 
+
+                        Menu open: "hidden", Menu closed: "block"
+                    -->
+                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                    <!-- 
+                        Icon when menu is open. 
+
+                        Menu open: "block", Menu closed: "hidden"
+                    -->
+                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                 </button>
+            </div>
+        </div>
+    </nav>
+</template>
