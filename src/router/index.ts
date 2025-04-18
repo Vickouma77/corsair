db934d41dee3594edef6c700fd1cc440a316c678
Vickouma77 <Vickprestone20@gmail.com>
2025-04-18 17:42:06 +0300

    feat: update job routing and add JobsView component for job details display

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobVIew from "@/views/JobVIew.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobsView from "@/views/JobsView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
             path: "/jobs",
             name: "jobs",
             component: JobVIew
+        },
+        {
+            path: "/jobs/:id",
+            name: "job",
+            component: JobsView
         },
         {
             path: "/:catchAll(.*)",
